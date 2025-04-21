e692a167e49a82fec4d33c2a2bc4553d1944d7dd
eliad <eliad.haim@gmail.com>
2025-04-21 12:53:56 +0300

    Add toy service

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import { loggerService } from './services/logger.service.js'
+import { toyService } from './services/toy.service.js'
+
 import express from 'express'
 
 const app = express()
@@ -6,6 +9,89 @@ app.use(express.static('public'))
 app.use(express.json())
 
 
+// toy api
+
+app.get('/api/toy', (req, res) => {
+
+    const filterBy = {
+        name: req.query.name || '',
+        price: +req.query.price || 0,
+        labels: req.query.labels || [],
+        pageIdx: +req.query.pageIdx || 0,
+        sortType: req.query.sortType || '',
+        sortDir: req.query.sortDir || 0,
+    }
+
+
+    toyService.query(filterBy)
+        .then(data => res.send(data))
+        .catch(err => {
+            loggerService.error('cannot load toys', err)
+            res.status(500).send('cannot load toys')
+        })
+})
+
+app.get('/api/toy/:toyId', (req, res) => {
+    const { toyId } = req.params
+
+    toyService.get(toyId)
+        .then(toy => res.send(toy))
+        .catch(err => {
+            loggerService.error('cannot load toy', err)
+            res.status(500).send('cannot load toy')
+        })
+})
+
+app.post('/api/toy', (req, res) => {
+    const { name, price, imgUrl, labels } = req.body
+    if (!name || !price) return res.status(400).send('Missing required fields')
+
+    const toyToSave = {
+        name,
+        price: +price || 0,
+        imgUrl: imgUrl || '',
+        labels: labels || [],
+    }
+
+    toyService.save(toyToSave)
+        .then(toy => res.send(toy))
+        .catch(err => {
+            loggerService.error('cannot save toy', err)
+            res.status(500).send('cannot save toy')
+        })
+})
+
+app.put('/api/toy/:toyId', (req, res) => {
+    const { _id, name, price, imgUrl, labels, inStock } = req.body
+    if (!_id || !name || !price) return res.status(400).send('Missing required fields')
+
+    const toyToSave = {
+        _id,
+        name,
+        price: +price || 0,
+        imgUrl: imgUrl || '',
+        labels: labels || [],
+        inStock: inStock || true
+    }
+
+    toyService.save(toyToSave)
+        .then(toy => res.send(toy))
+        .catch(err => {
+            loggerService.error('cannot save toy', err)
+            res.status(500).send('cannot save toy')
+        })
+})
+
+app.delete('/api/toy/:toyId', (req, res) => {
+    const { toyId } = req.params
+
+    toyService.remove(toyId)
+        .then(toy => res.send(toy))
+        .catch(err => {
+            loggerService.error('cannot remove toy', err)
+            res.status(500).send('cannot remove toy')
+        })
+})
 
 
 
---
+++ services/toy.service.js
@@ -0,0 +1,97 @@
+import { utilService } from "./util.service.js"
+import fs from 'fs'
+
+
+export const toyService = {
+    query,
+    get,
+    remove,
+    save,
+}
+
+const toys = utilService.readJsonFile('data/toy.json')
+const PAGE_SIZE = 12
+
+function query(filterBy) {
+
+    return Promise.resolve(toys)
+        .then(toys => {
+
+
+            if (filterBy.name) {
+                const regExp = new RegExp(filterBy.name, 'i')
+                toys = toys.filter(toy => regExp.test(toy.name))
+            }
+
+            if (filterBy.price) {
+                toys = toys.filter(toy => toy.price >= filterBy.price)
+            }
+
+            if (filterBy.inStock !== undefined) {
+                toys = toys.filter(toy => toy.inStock === filterBy.inStock)
+            }
+
+            if (filterBy.labels && filterBy.labels.length > 0) {
+                toys = toys.filter(toy => {
+                    return toy.labels.some(label => filterBy.labels.includes(label))
+                })
+            }
+
+
+            if (filterBy.sortType === 'name') {
+                toys = toys.sort((t1, t2) => (t1.name.localeCompare(t2.name)) * filterBy.sortDir)
+            }
+            if (filterBy.sortType === 'price') {
+                toys = toys.sort((t1, t2) => (t1.price - t2.price) * filterBy.sortDir)
+            }
+            if (filterBy.sortType === 'createdAt') {
+                toys = toys.sort((t1, t2) => (t1.createdAt - t2.createdAt) * filterBy.sortDir)
+            }
+
+            const maxPageCount = Math.ceil(toys.length / PAGE_SIZE)
+
+
+            const startIdx = filterBy.pageIdx * PAGE_SIZE
+            toys = toys.slice(startIdx, startIdx + PAGE_SIZE)
+
+
+            return { toys, maxPageCount }
+        })
+}
+function get(toyId) {
+    const toy = toys.find(toy => toy._id === toyId)
+    if (!toy) return Promise.reject('cannot find toy:' + toyId)
+    return Promise.resolve(toy)
+}
+function remove(toyId) {
+    const toyIdx = toys.findIndex(toy => toy._id === toyId)
+    if (toyIdx === -1) return Promise.reject('cannot find toy:' + toyId)
+    toys.splice(toyIdx, 1)
+    return _saveToysToFile()
+}
+function save(toyToSave) {
+    if (toyToSave._id) {
+        const toyIdx = toys.findIndex(toy => toy._id === toyToSave._id)
+        toys[toyIdx] = { ...toys[toyIdx], ...toyToSave }
+
+    } else {
+        toyToSave._id = utilService.makeId()
+        toyToSave.createdAt = Date.now()
+        toyToSave.inStock = true
+        toys.unshift(toyToSave)
+    }
+    return _saveToysToFile().then(() => toyToSave)
+}
+
+
+function _saveToysToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(toys, null, 4)
+        fs.writeFile('data/toy.json', data, (err) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
